91a701b2c8eb815f7928c20f4a3750ea48f5808c
Adrian Cubillos <adriancubillos@gmail.com>
2024-11-21 13:56:23 -0500

    Added items for about page

--- src/app/components/about/index.jsx
+++ src/app/components/about/index.jsx
@@ -34,8 +34,46 @@ const AboutDetails = () => {
           <div className='font-semibold w-full text-left text-5xl'>
             15+ <sub className='font-semibold text-base'>years of experience</sub>
           </div>
-
         </ItemLayout>
+
+        <ItemLayout className={'col-span-8 !p-0'}>
+          <img
+            className="w-full h-auto"
+            src='https://github-readme-stats.vercel.app/api?username=adriancubillos&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&custom_title=Adriancubillos Github Stats'
+            alt='Github Stats Image'
+            loading='lazy'></img>
+        </ItemLayout>
+
+        <ItemLayout className={'col-span-4 !p-0'}>
+          <img
+            className="w-full h-auto"
+            src='https://github-readme-stats.vercel.app/api/top-langs?username=adriancubillos&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF'
+            alt='Github Stats Image'
+            loading='lazy'></img>
+        </ItemLayout>
+
+        <ItemLayout className={'col-span-full'}>
+          <img
+            className="w-full h-auto"
+            src='https://skillicons.dev/icons?i=js,ts,angular,react,redux,nextjs,vue,express,nestjs,graphql,postman,nodejs,npm,html,css,tailwind,sass,aws,docker,heroku,github,bitbucket,firebase,mongodb,postgres,ai,py,java,spring,jenkins,vscode,eclipse,jest,selenium,ubuntu,linux,maven,webpack,stackoverflow'
+            alt='Github Skill Inventory Image'
+            loading='lazy'></img>
+        </ItemLayout>
+
+        <ItemLayout className={'col-span-6 !p-0'}>
+          <img
+            className="w-full h-auto"
+            src="https://github-readme-streak-stats.herokuapp.com?user=adriancubillos&theme=dark&hide_border=true&type=png&background=EB545400&ring=FEFE5B&currStreakLabel=FEFE5B" alt="GitHub Streak" loading='lazy'></img>
+        </ItemLayout>
+
+        <ItemLayout className={'col-span-6 !p-0'}>
+          <img
+            className="w-full h-auto"
+            src='https://github-readme-stats.vercel.app/api/pin?username=adriancubillos&repo=InteractivePortfolio&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF'
+            alt='Github Readme Stats'
+            loading='lazy'></img>
+        </ItemLayout>
+
 
 
       </div>
