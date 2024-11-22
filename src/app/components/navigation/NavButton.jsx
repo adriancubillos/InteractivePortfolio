3c7ce916755020b18bed37f648007eed2cb6ae53
Adrian Cubillos <adriancubillos@gmail.com>
2024-11-22 12:14:19 -0500

    Increase menu readio based on screen size and added static menu for small screens

--- src/app/components/navigation/NavButton.jsx
+++ src/app/components/navigation/NavButton.jsx
@@ -1,6 +1,8 @@
 import { FileSliders, FileStack, Github, Home, LinkedinIcon, NotebookText, Phone, User } from 'lucide-react';
 import Link from 'next/link'
 import React from 'react'
+import ResponsiveComponent from '../ResponsiveComponent';
+import clsx from 'clsx';
 
 const getIcon = (iconName) => {
   switch (iconName) {
@@ -28,25 +30,49 @@ const getIcon = (iconName) => {
 
 const NavButton = ({ x, y, label, link, icon, newTab }) => {
   return (
-    <div className='absolute cursor-pointer z-50'
-      style={{ transform: `translate(${x}, ${y})`, position: 'absolute', color: 'white', fontSize: '1.5rem', fontWeight: 'bold' }}>
+    <ResponsiveComponent>
+      {
+        ({ size }) => {
+          return size && size >= 480 ?
+            <div className='absolute cursor-pointer z-50'
+              style={{ transform: `translate(${x}, ${y})` }}>
 
-      <Link href={link} target={newTab ? '_blank' : "_self"}
-        className='text-foreground rounded-full flex items-center justify-center custom-bg'
+              <Link href={link} target={newTab ? '_blank' : "_self"}
+                className='text-foreground rounded-full flex items-center justify-center custom-bg'
+                aria-label={label}
+                name={label}>
+                <span className='relative w-14 h-14 p-4 animate-spin-slow-reverse hover:text-accent group-hover:pause'>
+                  {getIcon(icon)}
+                  <span className='peer absolute top-0 left-0 w-full h-full' />
+                  <span className='absolute hidden peer-hover:block px-2 py-1 left-full mx-2 top-1/2 -translate-y-1/2 bg-background text-foreground text-sm rounded-md shadow-lg'>
+                    {label}
+                  </span>
+                </span>
+              </Link>
+            </div>
+            :
+            <div className='w-fit cursor-pointer z-50'
+              style={{ transform: `translate(${x}, ${y})` }}>
 
-        aria-label={label}
-        name={label}>
-        <span className='relative w-14 h-14 p-4 animate-spin-slow-reverse hover:text-accent group-hover:pause'>
-          {getIcon(icon)}
-          <span className='peer absolute top-0 left-0 w-full h-full' />
-          <span className='absolute hidden peer-hover:block px-2 py-1 left-full mx-2 top-1/2 -translate-y-1/2 
-          bg-background text-foreground text-sm rounded-md shadow-lg'>
-            {label}
-          </span>
-        </span>
-      </Link>
+              <Link href={link} target={newTab ? '_blank' : "_self"}
+                className='text-foreground rounded-full flex items-center justify-center custom-bg'
+                aria-label={label}
+                name={label}>
+                <span className='relative w-10 h-10 xs:w-14 xs:h-14 p-2.5 xs:p-4 hover:text-accent'>
+                  {getIcon(icon)}
+                  <span className='peer bg-transparent absolute top-0 left-0 w-full h-full' />
+                  <span className={clsx('absolute hidden peer-hover:block px-2 py-1 left-full mx-2 top-1/2 -translate-y-1/2 bg-background text-foreground text-sm rounded-md shadow-lg whitespace-nowrap')}>
+                    {label}
+                  </span>
 
-    </div>
+                </span>
+              </Link>
+            </div>
+        }
+      }
+
+
+    </ResponsiveComponent>
   )
 }
 
--- src/app/components/navigation/index.jsx
+++ src/app/components/navigation/index.jsx
@@ -2,26 +2,67 @@
 import { BtnList } from '@/app/data';
 import React from 'react'
 import NavButton from './NavButton';
+import ResponsiveComponent from '../ResponsiveComponent';
+import useScreenSize from '../hooks/useScreenSize';
 
 const Navigation = () => {
 
   const angleIncrement = 360 / BtnList.length;
+  const size = useScreenSize();
+  const isLarge = size >= 1024;
+  const isMedium = size >= 768;
+  const isSmall = size < 480;
+
+  const getlistLenght = (list) => {
+    return Math.ceil(list.length / 2);
+  }
 
   return (
+
+
     <div className='w-full fixed h-screen flex items-center justify-center'>
-      <div className="w-max flex items-center justify-center relative animate-spin-slow hover:pause group">
+      <ResponsiveComponent>
         {
-          BtnList.map((btn, index) => {
+          ({ size }) => {
+            return !isSmall ?
+              <div className="w-max flex items-center justify-center relative hover:pause animate-spin-slow  group">
+                {
+                  BtnList.map((btn, index) => {
 
-            const angleRad = (angleIncrement * index) * (Math.PI / 180);
-            const radius = 'calc(20vw - 1rem)';
-            const x = `calc(${radius}*${Math.cos(angleRad)})`;
-            const y = `calc(${radius}*${Math.sin(angleRad)})`;
+                    const angleRad = (index * angleIncrement) * (Math.PI) / 180;
+                    const radius = isLarge
+                      ? "calc(20vw - 1rem)"
+                      : isMedium
+                        ? "calc(35vw - 1rem)"
+                        : "calc(40vw - 1rem)";
+                    const x = `calc(${radius}*${Math.cos(angleRad)})`;
+                    const y = `calc(${radius}*${Math.sin(angleRad)})`;
 
-            return <NavButton key={btn.label} x={x} y={y} {...btn} />
-          })
+                    return <NavButton key={btn.label} x={x} y={y} {...btn} />
+                  })
+                }
+              </div>
+              :
+              <>
+                {/* Menu split in two */}
+                <div className="w-full xs:w-max px-2.5 xs:p-0 flex flex-col space-y-4 items-start xs:items-center justify-center relative">
+                  {
+                    BtnList.slice(0, getlistLenght(BtnList)).map((btn, index) => {
+                      return <NavButton key={btn.label} x={0} y={0} {...btn} />
+                    })
+                  }
+                </div>
+                <div className="w-full xs:w-max px-2.5 xs:p-0 flex flex-col space-y-4 items-end xs:items-center justify-center relative">
+                  {
+                    BtnList.slice(getlistLenght(BtnList), BtnList.length).map((btn, index) => {
+                      return <NavButton key={btn.label} x={0} y={0} {...btn} />
+                    })
+                  }
+                </div>
+              </>
+          }
         }
-      </div>
+      </ResponsiveComponent>
     </div>
 
   )
