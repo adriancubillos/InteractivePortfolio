b37d86750709aa429a787f5e306fb1b502a6882f
Adrian Cubillos <adriancubillos@gmail.com>
2024-11-24 11:25:46 -0500

    Adding sound capabilities

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import clsx from "clsx";
 import FireFliesBackground from "./components/FireFliesBackground";
+import Sound from "./components/Sound";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -16,8 +17,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={inter.className}>
-      <body className={clsx(inter.variable, "bg-background text-foreground font-inter")}>{children}
+      <body className={clsx(inter.variable, "bg-background text-foreground font-inter")}>
+        {children}
         <FireFliesBackground />
+        <Sound />
+        <div id="my-modal" className="z-[9999] fixed inset-0 pointer-events-none"></div>
       </body>
     </html>
   );
