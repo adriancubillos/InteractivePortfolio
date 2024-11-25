4c44c2e66abb4e07eca52996a70824a060abacbb
Adrian Cubillos <adriancubillos@gmail.com>
2024-11-25 02:48:57 -0500

    fixing strange bug with about background

--- src/app/(sub pages)/about/page.js
+++ src/app/(sub pages)/about/page.js
@@ -12,8 +12,6 @@ export default function About() {
   return (
     <>
       <Image
-        priority
-        sizes="100vw"
         src={bg}
         alt="background-image"
         className="-z-50 fixed top-0 left-0 w-full h-full object-cover object-center opacity-50"
